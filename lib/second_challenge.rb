243115370e1bad597075a94c7d6803372215dfa7
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-24 13:50:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,7 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch,:matriarch,:hero,:hero_friends#your key/value pairs here!
+      [:patriarch,:matriarch,:hero,:hero_friends]#your key/value pairs here!
       },
     :capulet => {
       #your key/value pairs here!
